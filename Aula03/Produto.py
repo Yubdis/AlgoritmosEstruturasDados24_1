4d94d7332b5aa4715f5893d0821c1bda0d66a05a
SIMON KAR SING TSUI <632310191@fspoa.br>
2024-03-27 10:47:25 -0300

    Adding previous weeks

---
+++ Aula03/Produto.py
@@ -0,0 +1,16 @@
+from Categoria import Categoria
+class Produto:
+    def __init__(self, nome, preco=10.0, qtd = 0, cat=Categoria( None ) ):
+        self.nome = nome
+        self.preco = preco
+        self.qtd = qtd
+        self.cat = cat
+    def __str__(self):
+        texto = "Produto: " + self.nome 
+        texto += "\nPreço: " + str( self.preco) 
+        # texto += "\nCategoria: " + self.cat.nome 
+        texto += "\n" + str(self.cat) 
+        return texto
+
+
+

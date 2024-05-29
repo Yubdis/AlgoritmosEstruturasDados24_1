fbb599921dae9ea4f48b2d1bb7d96ae055f7fe9f
Simon <simontsui85@gmail.com>
2024-05-29 11:40:42 -0300

    Aula09 - Lista Encadeada

--- Aula08 - Atividade/Desktop.py
+++ Aula08 - Atividade/Desktop.py
@@ -1,24 +1,24 @@
 from Produto import *
 
-# 2. O projeto também deve possuir as classes Desktop e Notebook que herdam da classe Produto. 
-# A classe Desktop possui o atributo/propriedade fracamente privado potenciaDaFonte. 
+# 2. O projeto também deve possuir as classes Desktop e Notebook que herdam da classe Produto.
+# A classe Desktop possui o atributo/propriedade fracamente privado potenciaDaFonte.
 # Esta classe reimplementa o método getInformacoes() herdado de Produto.
 
 class Desktop(Produto):
-    def __init__(self, modelo, cor, preco, categoria, _potencialDaFonte):
+    def __init__(self, modelo, cor, preco, categoria, potencialDaFonte):
         super().__init__(modelo, cor, preco, categoria)
-        self._potencialDaFonte = _potencialDaFonte
-    
+        self._potencialDaFonte = potencialDaFonte
+
     def getInformacoes(self):
         print("=====Desktop=====")
         super().getInformacoes()
         print("\nPotencial Da Fonte: " + str(self.getPotencialDeBateria()) )
-        
+
     def getPotencialDeBateria(self):
         return self._potencialDaFonte
-        
+
     def setPotencialDeBateria(self, valor):
         self._potencialDaFonte = valor
-        
+
     def cadastrar(self, nome):
         print("Desktop Registrou para: " + nome)
---
+++ Aula09 - ListaEncadeada/main.py
@@ -0,0 +1,16 @@
+from Lista import Lista
+
+lista = Lista()
+
+lista.imprimir()
+
+lista.adicionarNoInicio("Joao")
+lista.adicionarNoFim("Maria")
+lista.adicionarNoInicio("Julia")
+lista.adicionarNoFim("Jose")
+
+lista.removerDoFim()
+lista.removerDoInicio()
+lista.removerDoFim()
+lista.removerDoInicio()
+lista.removerDoFim()

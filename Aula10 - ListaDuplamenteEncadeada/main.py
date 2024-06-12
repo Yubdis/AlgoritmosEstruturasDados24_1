734056fa7aa4bde7cf41b22b6b6ac86c7c4ca323
SIMON KAR SING TSUI <632310191@fspoa.br>
2024-06-12 10:27:05 -0300

    Lista Duplamente Encadeada

---
+++ Aula10 - ListaDuplamenteEncadeada/main.py
@@ -0,0 +1,19 @@
+from ListaDuplamente import ListaDuplamente
+
+lista = ListaDuplamente()
+
+lista.imprimir()
+
+lista.add("Simon")
+lista.add("Maria")
+lista.add("Zeus")
+lista.add("Adão")
+
+lista.imprimirReverso()
+
+
+lista.remover("Simon")
+lista.remover("Adão")
+lista.remover("Maria")
+
+lista.imprimir()
